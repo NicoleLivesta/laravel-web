4409c39629c7bc0002e155b6514b6ae2e5efc716
momoriarty <234307350+fransiskus24ti-svg@users.noreply.github.com>
2025-10-13 16:13:34 +0700

    voltTemplate

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -41,3 +42,6 @@ Route::get('/home', [HomeController::class,'index'])->name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+
