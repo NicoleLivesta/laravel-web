704a09b14a49709d7776ee3bf255f7bfc6c299ef
frnsbpf <nicole24ti@mahasiswa.pcr.ac.id>
2025-11-24 15:36:25 +0700

    Update PelangganController.php

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -12,10 +12,15 @@ class PelangganController extends Controller
     public function index(Request $request)
     {
         $filterableColumns = ['gender'];
-        $searchableColumns = ['first_name','last_name','email'];
-        $pageData['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)->paginate(10)->withQueryString();
-        $data['dataPelanggan'] = Pelanggan::filter($request,$filterableColumns)->search($request, $searchableColumns)->paginate(10)->withQueryString();
-        return view('admin.pelanggan.index', $pageData);
+        $searchableColumns = ['first_name', 'last_name', 'email'];
+
+        $data['dataPelanggan'] = Pelanggan::query()
+            ->filter($request, $filterableColumns)
+            ->search($request, $searchableColumns)
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('admin.pelanggan.index', $data);
     }
 
     /**
